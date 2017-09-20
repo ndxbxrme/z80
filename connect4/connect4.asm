418cadf9a9e8cad54c3fdae9ab4674478874a14e
ndxbxrme <lewis_the_cat@hotmail.com>
2017-09-20 12:40:22 +0100

    drop counters

--- connect4/connect4.asm
+++ connect4/connect4.asm
@@ -5,17 +5,33 @@ paddr	equ 22816
 	call 8859
 	ld hl, udgs
 	ld (23675), hl
-	ld a, 2
+	;fancy font
+	ld hl,15616		; ROM font.
+	ld de,60000		; address of our font.
+	ld bc,768		; 96 chars * 8 rows to alter.
+font1	ld a,(hl)		; get bitmap.
+	rlca			; rotate it left.
+	or (hl)			; combine 2 images.
+	ld (de),a		; write to new font.
+	inc hl			; next byte of old.
+	inc de			; next byte of new.
+	dec bc			; decrement counter.
+	ld a,b			; high byte.
+	or c			; combine with low byte.
+	jr nz,font1		; repeat until bc=zero.
+	ld hl,60000-256		; font minus 32*8.
+	ld (23606),hl		; point to new font.
+	ld a, 2			; move to upper left
 	call 5633
-	ld de, string
+	ld de, string		; write Connect 4
 	ld bc, eostr-string
 	call wstr
-	ld de, btop
+	ld de, btop		; top of board
 	ld bc, eobtop-btop
 	call wstr
 	ld b, 6
 	ld c, 10
-loop	ld a, 22
+loop	ld a, 22		; draw row of board
 	rst 16
 	ld a, c
 	push bc
@@ -26,7 +42,7 @@ loop	ld a, 22
 	pop bc
 	inc c
 	djnz loop
-ploop	ld hl, paddr
+ploop	ld hl, paddr		; main loop draws counter and checks for keypresses
 	ld bc, (ppos)
 	add hl, bc
 	ld a, (pcol)
@@ -40,7 +56,7 @@ ploop	ld hl, paddr
 	cp 110
 	jp z, pdrop
 	jr ploop
-pright	ld a, (ppos)
+pright	ld a, (ppos)		; move player cursor to the right
 	cp 18
 	jr z, ploop
 	ld hl, paddr
@@ -51,7 +67,7 @@ pright	ld a, (ppos)
 	inc a
 	ld (ppos), a
 	jp clrkey
-pleft	ld a, (ppos)
+pleft	ld a, (ppos)		; move player cursor to the left
 	cp 12
 	jr z, ploop
 	ld hl, paddr
@@ -62,17 +78,17 @@ pleft	ld a, (ppos)
 	dec a
 	ld (ppos), a
 	jp clrkey
-pdrop	ld hl, paddr
+pdrop	ld hl, paddr		; drop counter
 	ld bc, (ppos)
 	add hl, bc
 	ld b, 6
-dloop	push bc
+dloop	push bc			; loop to move counter down
 	push hl
 	ld bc, 32
 	add hl, bc
 	ld a, (hl)
 	cp 15
-	jp nz, clrkey
+	jr nz, switch
 	pop hl
 	ld a, 15
 	ld (hl), a
@@ -81,17 +97,15 @@ dloop	push bc
 	ld (hl), a
 	pop bc
 	djnz dloop
-	ld hl, p1col
+switch	ld hl, p1col		; switch from p1 to p2 and back
 	ld a, (hl)
 	ld hl, pcol
 	ld b, (hl)
 	cp b
 	jr nz, p1up
 	jr p2up
-	jp clrkey
-p1up	ld hl, p1col
-	ld a, "1"
-	rst 16
+	jr clrkey
+p1up	ld hl, p1col		; set player 1 active
 	ld b, (hl)
 	inc hl
 	ld c, (hl)
@@ -99,10 +113,8 @@ p1up	ld hl, p1col
 	ld (hl), b
 	inc hl
 	ld (hl), c
-	jp clrkey
-p2up	ld hl, p2col
-	ld a, "2"
-	rst 16
+	jr clrkey
+p2up	ld hl, p2col		; set player 2 active
 	ld b, (hl)
 	inc hl
 	ld c, (hl)
@@ -110,8 +122,8 @@ p2up	ld hl, p2col
 	ld (hl), b
 	inc hl
 	ld (hl), c
-	jp clrkey
-clrkey	ld hl, last_k
+	jr clrkey
+clrkey	ld hl, last_k		; clears last keypress and returns to ploop
 	ld (hl), 0
 	jp ploop
 	ret
@@ -128,4 +140,3 @@ pcol	defb 58, 10
 p1col	defb 58, 10
 p2col	defb 62, 14
 udgs	defb 0, 24, 60, 126, 126, 60, 24, 0
-	
